1f9b00cb0e7d3f1567488a78c8f8257dbef6aa84
Michal Rostecki <vadorovsky@protonmail.com>
2024-02-24 15:59:19 +0100

    [SOL] build: Allow cross-compilation
    Rename `HOST_TRIPLE` to `TARGET_TRIPLE`. If it's already defined,
    do not override. This way, we are allowing cross-compilation.

--- build.sh
+++ build.sh
@@ -2,22 +2,26 @@
 
 set -ex
 
+TARGET_TRIPLE=${TARGET_TRIPLE:-}
+
 if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     echo "--llvm to rebuild llvm";
     exit;
 fi
 
-unameOut="$(uname -s)-$(uname -m)"
-case "${unameOut}" in
-    Linux-x86_64*)  HOST_TRIPLE=x86_64-unknown-linux-gnu;;
-    Linux-aarch64*) HOST_TRIPLE=aarch64-unknown-linux-gnu;;
-    Darwin-x86_64*) HOST_TRIPLE=x86_64-apple-darwin;;
-    Darwin-arm64*)  HOST_TRIPLE=aarch64-apple-darwin;;
-    MINGW*)         HOST_TRIPLE=x86_64-pc-windows-msvc;;
-    *)              HOST_TRIPLE=x86_64-unknown-linux-gnu
-esac
+if [[ -z "${TARGET_TRIPLE}" ]]; then
+    unameOut="$(uname -s)-$(uname -m)"
+    case "${unameOut}" in
+        Linux-x86_64*)  TARGET_TRIPLE=x86_64-unknown-linux-gnu;;
+        Linux-aarch64*) TARGET_TRIPLE=aarch64-unknown-linux-gnu;;
+        Darwin-x86_64*) TARGET_TRIPLE=x86_64-apple-darwin;;
+        Darwin-arm64*)  TARGET_TRIPLE=aarch64-apple-darwin;;
+        MINGW*)         TARGET_TRIPLE=x86_64-pc-windows-msvc;;
+        *)              TARGET_TRIPLE=x86_64-unknown-linux-gnu
+    esac
+fi
 
 if [ "$1" == "--llvm" ]; then
-    rm -f build/${HOST_TRIPLE}/llvm/llvm-finished-building;
+    rm -f build/${TARGET_TRIPLE}/llvm/llvm-finished-building;
 fi
-./x.py build --stage 1 --target ${HOST_TRIPLE},bpfel-unknown-unknown,sbf-solana-solana
+./x.py build --stage 1 --target ${TARGET_TRIPLE},bpfel-unknown-unknown,sbf-solana-solana
