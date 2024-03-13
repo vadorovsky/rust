696ef741a6b654a4fe9f46bc47b502049e919aa9
Lucas Steuernagel <38472950+LucasSte@users.noreply.github.com>
2024-03-13 10:35:55 -0300

    [SOL] Update SBF call ABI (#91)

--- compiler/rustc_target/src/abi/call/sbf.rs
+++ compiler/rustc_target/src/abi/call/sbf.rs
@@ -1,23 +1,33 @@
 // see https://github.com/llvm/llvm-project/blob/main/llvm/lib/Target/BPF/BPFCallingConv.td
-use crate::abi::call::{ArgAbi, FnAbi};
+use crate::abi::call::{ArgAbi, FnAbi, Reg, Uniform};
 
 fn classify_ret<Ty>(ret: &mut ArgAbi<'_, Ty>) {
-    if ret.layout.is_aggregate() || ret.layout.size.bits() > 64 {
-        if ret.layout.size.bits() != 128 {
-            ret.make_indirect();
-        }
-    } else {
+    let size = ret.layout.size;
+    let bits = size.bits();
+    if !ret.layout.is_aggregate() && bits <= 64 {
         ret.extend_integer_width_to(64);
+        return;
+    }
+
+    if bits <= 128 {
+        ret.cast_to(Uniform {unit: Reg::i64(), total: size});
+    } else {
+        ret.make_indirect();
     }
 }
 
 fn classify_arg<Ty>(arg: &mut ArgAbi<'_, Ty>) {
-    if arg.layout.is_aggregate() || arg.layout.size.bits() > 64 {
-        if arg.layout.size.bits() != 128 {
-            arg.make_indirect();
-        }
-    } else {
+    let size = arg.layout.size;
+    let bits = size.bits();
+    if !arg.layout.is_aggregate() && bits <= 64 {
         arg.extend_integer_width_to(64);
+        return;
+    }
+
+    if bits <= 128 {
+        arg.cast_to(Uniform {unit: Reg::i64(), total: size});
+    } else {
+        arg.make_indirect();
     }
 }
 
